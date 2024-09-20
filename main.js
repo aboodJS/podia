e9a3ab3565786631478ff6795a48e39074d1e2b5
ajs <140010746+aboodJS@users.noreply.github.com>
2024-09-20 18:41:52 +0300

    added some styling to the nav bar

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-const nav = document.querySelector('nav')
+const nav = document.querySelector("nav");
+const listItems = document.querySelectorAll("nav #menus li");
 
 window.onscroll = () => {
-    let yaxis = window.scrollY
-    if ( yaxis >= 100) {
-        nav.style.boxShadow = '0 2px 10px 0px black'
-        nav.style.transitionDuration = '200ms'
-    }else {
-        nav.style.boxShadow = 'none'
+  let yaxis = window.scrollY;
+  if (yaxis >= 100) {
+    nav.style.boxShadow = "0 2px 10px 0px black";
+    nav.style.transitionDuration = "200ms";
+  } else {
+    nav.style.boxShadow = "none";
+  }
+  console.log(yaxis);
+};
 
+console.log(listItems);
+
+listItems.forEach((el) => {
+  el.addEventListener("click", () => {
+    for (let i = 0; i < listItems.length; i++) {
+      listItems[i].classList.remove("focus");
     }
-    console.log(yaxis)
-}
+    el.classList.add("focus");
+  });
+});
