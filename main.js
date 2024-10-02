f86fd5ac24fb322cdabf90a0a42beba254c21316
ajs <140010746+aboodJS@users.noreply.github.com>
2024-10-02 20:59:32 +0300

    added submenus in the navbar

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 const nav = document.querySelector("nav");
 const listItems = document.querySelectorAll("nav #menus li");
+const subMenus = document.querySelectorAll(".sub-menu");
+const subDivs = document.getElementsByClassName(".sub-menu div");
 
 window.onscroll = () => {
   let yaxis = window.scrollY;
   if (yaxis >= 100) {
     nav.style.boxShadow = "0 2px 10px 0px black";
-    nav.style.transitionDuration = "200ms";
   } else {
     nav.style.boxShadow = "none";
   }
   console.log(yaxis);
 };
 
-console.log(listItems);
-
-listItems.forEach((el) => {
+listItems.forEach((el, i) => {
   el.addEventListener("click", () => {
     for (let i = 0; i < listItems.length; i++) {
       listItems[i].classList.remove("focus");
     }
     el.classList.add("focus");
+    subMenus.forEach((elem, i) => {
+      if (elem.classList.contains("focus")) {
+        elem.children[1].style.display = "block";
+      } else {
+        elem.children[1].style.display = "none";
+      }
+    });
   });
 });
